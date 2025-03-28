a63e2409d17f896818573d8580183cc5129481aa
odinaka-dev <uchechukwupeter22@gmail.com>
2025-03-28 12:45:51 +0100

    Update taskController.js

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -27,6 +27,23 @@ const getTasks = async (req, res) => {
   }
 };
 
+//@route GET /api/tasks/:id
+const getTaskById = async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+    
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+
+    res.status(200).json(task);
+  } catch (error) {
+    console.error("Error fetching task:", error);
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+};
+
+
 // @route   PUT /api/tasks/:id
 const updateTask = async (req, res) => {
   try {
@@ -57,4 +74,4 @@ const deleteTask = async (req, res) => {
   }
 };
 
-module.exports = { createTask, getTasks, updateTask, deleteTask };
+module.exports = { createTask, getTasks, getTaskById, updateTask, deleteTask };
